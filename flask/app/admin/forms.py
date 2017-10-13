64143bf9092f5462014796d0134157ba0746837d
Lee Sives <leesives@live.co.uk>
2017-10-13 15:37:06 +0100

    Added new variables

--- flask/app/admin/forms.py
+++ flask/app/admin/forms.py
@@ -1,13 +1,21 @@
 from flask_wtf import Form
 from wtforms import StringField
 from wtforms import IntegerField
+from wtforms import FileField
 from wtforms.validators import DataRequired
 
 class LocationPageForm(Form):
     name = StringField('name', validators=[DataRequired()])
     description = StringField('description', validators=[DataRequired()])
+    image = FileField('image', validators=[DataRequired()])
+    image_alt = StringField('image_alt', validators=[DataRequired()])
+    lat = StringField('lat', validators=[DataRequired()])
+    lon = StringField('lon', validators=[DataRequired()])
+
 
 class CoursePageForm(Form):
     name = StringField('name', validators=[DataRequired()])
     description = StringField('description', validators=[DataRequired()])
     price = IntegerField('price', validators=[DataRequired()])
+
+
--- flask/app/admin/models.py
+++ flask/app/admin/models.py
@@ -4,6 +4,10 @@ class LocationPage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     description = db.Column(db.String(1000))
+    image = db.Column(db.String())
+    image_alt = db.Column(db.String())
+    lat = db.Column(db.String())
+    lon = db.Column(db.String())
 
 class CoursePage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
